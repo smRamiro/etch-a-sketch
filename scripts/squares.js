d9d737d90326686d887e97309ed3d655f3518367
sRamiro <ramiro_sando@outlook.com>
2020-04-26 11:53:42 -0300

    mouseover event added

--- scripts/squares.js
+++ scripts/squares.js
@@ -5,3 +5,15 @@ while (counter < 256){
     document.querySelector(".squareContainer").appendChild(div).setAttribute("class","square");
     ++counter;
 }
+
+const squares = document.querySelectorAll(".square");
+
+for (let i = 0; i < squares.length; i++) {
+    squares[i].addEventListener("mouseover", changeColor);
+}
+
+function changeColor(e){
+    let randomColor = Math.floor(Math.random()*16777215).toString(16);
+    e.toElement.style.backgroundColor = `#${randomColor}`; // e = event. "e.toElement" = event --> element triggered.
+
+}
